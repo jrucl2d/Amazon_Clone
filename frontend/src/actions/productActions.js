2bb4d8d784c59cfb38f939efc97434acfaf2cb11
jrucl2d <ysk789465@gmail.com>
2020-10-09 15:14:45 +0900

    Using redux

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} = require("../constants/productConstants");
+
+const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get("/api/products");
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({ type: PRODUCT_LIST_FAIL, payload: err.message });
+  }
+};
+export { listProducts };
