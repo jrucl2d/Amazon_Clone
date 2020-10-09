adb0216e976fe3b9fe63f5c55a311e9a93a859a7
jrucl2d <ysk789465@gmail.com>
2020-10-09 14:01:12 +0900

    server setting

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import data from './data'
+const app = express();
+
+app.get('/api/products', (req,res, next) => {
+    res.send(data.products);
+})
+
+
+app.listen(8000, ()=>{
+    console.log("server is running on port 8000")
+})
