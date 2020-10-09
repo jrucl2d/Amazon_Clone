48e18b4740bb2e7f72615a0dfb6685bc9230b1a4
jrucl2d <ysk789465@gmail.com>
2020-10-09 13:04:06 +0900

    Modified static product component into dynamic data

---
+++ frontend/src/data.js
@@ -0,0 +1,14 @@
+const data = {  
+    products : [
+        {
+            name:'Slim Shirt',
+            category: 'shirt',
+            image:'/images/d1.jpg',
+            brand:'Nike',
+            price: 60,
+            rating:4.5,
+            numReviews:10,
+        }
+    ]
+}
+export default data;
