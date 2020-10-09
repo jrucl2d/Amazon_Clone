17958081b870946be07da30a8d9f22655901a7a6
jrucl2d <ysk789465@gmail.com>
2020-10-09 14:36:20 +0900

    Proxy setting and axios fetching data added

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
-import express from 'express'
-import data from './data'
+import express from "express";
+import data from "./data";
 const app = express();
 
-app.get('/api/products', (req,res, next) => {
-    res.send(data.products);
-})
+app.get("/api/products", (req, res, next) => {
+  res.send(data.products);
+});
+app.get("/", (req, res, next) => {
+  console.log("zz");
+});
 
-
-app.listen(8000, ()=>{
-    console.log("server is running on port 8000")
-})
+app.listen(8000, () => {
+  console.log("server is running on port 8000");
+});
