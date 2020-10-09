a2f52469bdc1b9b39ca198892cd39008e4dadc87
jrucl2d <ysk789465@gmail.com>
2020-10-09 13:47:59 +0900

    Added product detail window

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,11 +1,32 @@
 const data = {  
     products : [
         {
+            _id: "1",
             name:'Slim Shirt',
             category: 'shirt',
             image:'/images/d1.jpg',
             brand:'Nike',
             price: 60,
+            rating:4.5,
+            numReviews:10,
+        },
+        {
+            _id: "2",
+            name:'Fat Shirt',
+            category: 'shirt',
+            image:'/images/d1.jpg',
+            brand:'Nike',
+            price: 61230,
+            rating:4.5,
+            numReviews:10,
+        },
+        {
+            _id: "3",
+            name:'Slim Pants',
+            category: 'pants',
+            image:'/images/d1.jpg',
+            brand:'MansHealth',
+            price: 870,
             rating:4.5,
             numReviews:10,
         }
