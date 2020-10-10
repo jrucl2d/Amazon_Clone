3d21cdd3bdb028eb2458c3ae6bb898c9c33eb59b
jrucl2d <ysk789465@gmail.com>
2020-10-10 13:14:04 +0900

    Cart adding system done

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 function cartReducer(state = { cartItems: [] }, action) {
   switch (action.type) {
     case CART_ADD_ITEM:
@@ -7,11 +7,15 @@ function cartReducer(state = { cartItems: [] }, action) {
       if (product) {
         return {
           cartItems: state.cartItems.map(
-            (x) => (x.product === product.product ? product : item) // 또 넣는거면 해당 아이템의 수량을 추가
+            (x) => (x.product === product.product ? item : x) // 또 넣는거면 해당 아이템의 수량을 추가
           ),
         };
       }
       return { cartItems: [...state.cartItems, item] }; // 아니면 새로운 아이템을 넣음
+    case CART_REMOVE_ITEM:
+      return {
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
     default:
       return state;
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,8 +5,11 @@ import {
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import thunk from "redux-thunk";
+import Cookie from "js-cookie";
 
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: { cartItems } };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
