ab438a86382d56e0849ad668163a665aea6d1b32
jrucl2d <ysk789465@gmail.com>
2020-10-10 15:00:38 +0900

    signin with jwt in backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
+import getToken from "../util";
+
+router.post("/signin", async (req, res, next) => {
+  try {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (signinUser) {
+      res.send({
+        _id: signinUser.id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: getToken(user),
+      });
+    } else {
+      res.status(401).send({ msg: "Invalid Email or Password" });
+    }
+  } catch (error) {}
+});
 
 router.get("/createadmin", async (req, res) => {
   try {
